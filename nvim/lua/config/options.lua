72e9c60354351be4c11412ed2b0ee13d5eb278ab
Snuffish <snuffish90@gmail.com>
2024-12-03 21:57:09 +0100

    wip

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,6 +7,15 @@ local options = {
   relativenumber = true,
   showmode = false,
   mouse = "a",
+  fileencoding = "utf-8",
+  shiftwidth = 2,
+  tabstop = 2,
+  autoindent = false,
+  smartindent = true,
+  cursorlineopt = "both",
+
+  -- TODO: Fix a Lua-script to handle the vertical cursor-column later
+  cursorcolumn = false,
 }
 
 for option, value in pairs(options) do
