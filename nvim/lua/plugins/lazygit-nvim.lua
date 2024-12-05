893f16b56430fcc6f972a185c5e9ee5d1aa4f0d8
Snuffish <snuffish90@gmail.com>
2024-12-05 19:47:59 +0100

    wip

--- nvim/lua/plugins/lazygit-nvim.lua
+++ nvim/lua/plugins/lazygit-nvim.lua
@@ -11,7 +11,7 @@ return {
     "nvim-lua/plenary.nvim",
   },
   keys = {
-    { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
     { "<leader>sf", "<cmd>Telescope git_files<CR>", desc = "Git by [F]files" },
     { "<leader>sc", "<cmd>Telescope git_commits<CR>", desc = "Git by [C]ommits" },
   },
