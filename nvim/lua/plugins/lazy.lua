0390c2bef81de7212f10959807aac6a0118c324e
Snuffish <snuffish90@gmail.com>
2024-12-08 00:25:16 +0100

    wip

---
+++ nvim/lua/plugins/lazy.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+      { "<leader>sf", "<cmd>FzfLua git_files<CR>", desc = "Git by [F]files" },
+      { "<leader>sc", "<cmd>FzfLua git_commits<CR>", desc = "Git by [C]ommits" },
+    },
+  },
+  {
+    "mgierada/lazydocker.nvim",
+    dependencies = { "akinsho/toggleterm.nvim" },
+    config = function()
+      require("lazydocker").setup({
+        border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+      })
+    end,
+    event = "BufRead",
+    keys = {
+      {
+        "<leader>ld",
+        function()
+          require("lazydocker").open()
+        end,
+        desc = "Open LazyDocker",
+      },
+    },
+  },
+}
