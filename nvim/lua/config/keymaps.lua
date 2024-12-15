1e7fa388a04f138b87996526657f8cbeaaddb7f7
Snuffish <snuffish90@gmail.com>
2024-12-15 21:27:42 +0100

    wip

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,12 @@ local map = require("config.utils").map
 
 map("n", "<leader>a", "ggVG", { desc = "Select all text", silent = true })
 map("n", "<A-Up><A-Up>", ":<Up>", { desc = "Previous command", noremap = true, silent = true })
+map("x", { "/", "g/" }, "<esc>/\\%V", { silent = false, desc = "Search Inside Visual Selection" })
+
+-- Move cursor left/right in insert-mode
+map("ci", "<C-a>", "<Home>")
+map("ci", "<C-d>", "<End>")
+
 vim.api.nvim_set_keymap("n", "<localleader>z", "<leader>uz", { desc = "Toggle 'Zen Mode'" })
 
 -- Explorers
@@ -19,3 +25,11 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap("n", "sa", "gza", { desc = "Add surrounding" })
 vim.api.nvim_set_keymap("n", "ds", "gzd", { desc = "Delete surrounding" })
 vim.api.nvim_set_keymap("n", "cs", "gzc", { desc = "Change surrounding" })
+
+-- Regex replaces
+map("n", "RR", "<Esc>:%s/", { noremap = true, desc = "Regex string replace (global)" })
+map("x", "RR", "<Esc>:'<,'>s/", { noremap = true, desc = "Regex string replace (selection)" })
+
+-- Regex deletes
+map("n", "DD", "<Esc>:%s//<Left>", { noremap = true, desc = "Regex delete (selection)" })
+map("x", "DD", "<Esc>:'<,'>s//<Left>", { noremap = true, desc = "Regex delete (selection)" })
--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -2,19 +2,29 @@ local map = require("config.utils").map
 
 map("nxv", ";", ":", { noremap = true })
 map("nxv", ";;", ":<C-f>", { noremap = true })
-map("n", { "<PageUp>", "<C-u>zz" }, "<C-u>zz", { noremap = true, desc = "Jump up 1/2-screen" })
-map("n", { "<PageDown>", "<C-d>zz" }, "<C-d>zz", { noremap = true, desc = "Jump down 1/2-screen" })
+map("n", { "<PageUp>", "<C-u>zz" }, "<C-u>zz", { desc = "Jump up 1/2-screen" })
+map("n", { "<PageDown>", "<C-d>zz" }, "<C-d>zz", { desc = "Jump down 1/2-screen" })
 
-map("n", "{", "{zz", { noremap = true })
-map("n", "}", "}zz", { noremap = true })
+map("n", "{", "{zz")
+map("n", "}", "}zz")
 
-map("n", "n", "nzzzv", { noremap = true, desc = "Next search result (centered)" })
-map("n", "N", "Nzzzv", { noremap = true, desc = "Previous search result (centered)" })
+map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 
-map("n", "G", "Gzz", { noremap = true })
+map("n", "G", "Gzz")
 map("n", "GG", "Gzzo<CR>", { desc = "Goto last line and add 2 new lines" })
 
-map("x", "$", "g_", { noremap = true })
+-- Do not include white space characters when using $ in visual mode,
+map("x", "$", "g_")
+
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map("n", "^", "g^")
+map("n", "0", "g0")
+
+-- Go to start or end of line easier
+map("nx", "H", "g^")
+map("nx", "L", "g_")
 
 -- Delete without yanking
 map("n", "x", '"_x', { noremap = true, silent = true, desc = "Delete char (No yanking)" })
@@ -24,4 +34,9 @@ map("n", "d0", '"_g^d$', { noremap = true, silent = true, desc = "Delete line wi
 
 map("n", "DW", '"_db', { noremap = true, silent = true, desc = "Delete words backwards [inclusive] (No yanking)" })
 map("n", "DE", '"_dB', { noremap = true, silent = true, desc = "Delete WORDS backwards [exclusive] (No yanking)" })
-map("n", "CW", '"_T=<Space>cw', { noremap = true,silent = true, desc = "Change the rhs assignment of a declaration (No yanking)" })
+map(
+  "n",
+  "CW",
+  '"_T=<Space>cw',
+  { noremap = true, silent = true, desc = "Change the rhs assignment of a declaration (No yanking)" }
+)
