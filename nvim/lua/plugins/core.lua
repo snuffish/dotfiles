aec0e4aa66f3df0b9125ccfcef1c335eb4e9af5d
Snuffish <snuffish90@gmail.com>
2024-12-25 21:40:11 +0100

    wip

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -138,11 +138,4 @@ return {
       })
     end,
   },
-  {
-    "aaronik/treewalker.nvim",
-    opts = {
-      highlight = true, -- Whether to briefly highlight the node after jumping to it
-      highlight_duration = 250, -- How long should above highlight last (in ms)
-    },
-  },
 }
