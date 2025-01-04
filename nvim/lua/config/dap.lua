b6ee8e105de8f197f76c819b4f5fd1130c30c587
Snuffish <snuffish90@gmail.com>
2025-01-04 21:23:35 +0100

    wip

--- nvim/lua/config/dap.lua
+++ nvim/lua/config/dap.lua
@@ -1,37 +1,51 @@
+local dap = require("dap")
+local dapui = require("dapui")
+
+vim.keymap.set("n", "<F1>", function()
+  dap.step_into()
+end)
+
+vim.keymap.set("n", "<F2>", function()
+  dap.step_over()
+end)
+
+vim.keymap.set("n", "<F3>", function()
+  dap.step_out()
+end)
+
+vim.keymap.set("n", "<F4>", function()
+  dap.step_over()
+end)
+
 vim.keymap.set("n", "<F5>", function()
-  require("dap").continue()
+  dap.continue()
 end)
 
-vim.keymap.set("n", "<F10>", function()
-  require("dap").step_over()
-end)
+-- Keybinding to open the floating window
+vim.utils.map("n", "<F6>", vim.utils.trigger_keys_fn("<leader>dr<C-w>ji"), { noremap = true, silent = true })
 
-vim.keymap.set("n", "<F11>", function()
-  require("dap").step_into()
-end)
-
-vim.keymap.set("n", "<F12>", function()
-  require("dap").step_out()
+vim.keymap.set("n", "<Leader>d", function()
+  dap.continue()
 end)
 
 vim.keymap.set("n", "<Leader>b", function()
-  require("dap").toggle_breakpoint()
+  dap.toggle_breakpoint()
 end)
 
 vim.keymap.set("n", "<Leader>B", function()
-  require("dap").set_breakpoint()
+  dap.set_breakpoint()
 end)
 
 vim.keymap.set("n", "<Leader>lp", function()
-  require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+  dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 end)
 
 vim.keymap.set("n", "<Leader>dr", function()
-  require("dap").repl.open()
+  dap.repl.open()
 end)
 
 vim.keymap.set("n", "<Leader>dl", function()
-  require("dap").run_last()
+  dap.run_last()
 end)
 
 vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
