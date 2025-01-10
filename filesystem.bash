bcc94897683ea6c666010a2dbfeaa9205f60edd3
Snuffish <snuffish90@gmail.com>
2025-01-10 12:43:43 +0100

    wip

--- filesystem.bash
+++ filesystem.bash
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # Set defaults if not set above
 [[ $(aliasExists ".dl") -eq 0 ]] && alias .dl="cd $HOME/Downloads"
 [[ $(aliasExists ".ssh") -eq 0 ]] && alias .ssh="cd $HOME/.ssh"
-#!/bin/bash
 [[ $(aliasExists ".logs") -eq 0 ]] && alias .logs="cd $HOME/.logs"
 [[ $(aliasExists ".terminal") -eq 0 ]] && alias .terminal="cd $HOME/.terminal"
+[[ $(aliasExists ".t") -eq 0 ]] && alias .t="cd $HOME/.terminal"
 
 [[ $(aliasExists ".cd") -eq 0 ]] && alias .="cd $HOME"
 [[ $(aliasExists "..") -eq 0 ]] && alias ..="cd .."
