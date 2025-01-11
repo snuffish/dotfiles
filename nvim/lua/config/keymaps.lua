6450c218ee4f451e0da6fc75f56fca7ec8b8f717
Snuffish <snuffish90@gmail.com>
2025-01-11 13:37:42 +0100

    wip

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,39 +27,15 @@ vim.utils.map("x", { "?", "g?" }, "<esc>?\\%V", { silent = false, desc = "Search
 vim.utils.map("ic", "<C-a>", "<Home>")
 vim.utils.map("ic", "<C-d>", "<End>")
 
--- Regex replaces
--- vim.utils.map("n", "RR", "<Esc>:%s/", { noremap = true, desc = "Regex string replace (global)" })
--- vim.utils.map("x", "RR", "<Esc>:'<,'>s/", { noremap = true, desc = "Regex string replace (selection)" })
-
--- Regex deletes
--- vim.utils.map("n", "DD", "<Esc>:%s//<Left>", { noremap = true, desc = "Regex delete (selection)" })
--- vim.utils.map("x", "DD", "<Esc>:'<,'>s//<Left>", { noremap = true, desc = "Regex delete (selection)" })
-
 -- Mini.Surround mapping
-vim.api.nvim_set_keymap("", ";;", ";a_", { desc = "Add row surrounding" })
-vim.api.nvim_set_keymap("v", ";;", ";a", { desc = "Add surrounding" })
+vim.api.nvim_set_keymap("", "öa", ";a", { desc = "Add surrounding" })
+vim.api.nvim_set_keymap("", "öd", ";d", { desc = "Delete surrounding" })
+vim.api.nvim_set_keymap("", "öf", ";f", { desc = "Find right surrounding" })
+vim.api.nvim_set_keymap("", "öF", ";F", { desc = "Find left surrounding" })
+vim.api.nvim_set_keymap("", "ör", ";r", { desc = "Replace surrounding" })
 
--- Flash.nvim mapping
-local flash_line = function(forward)
-  return function()
-    require("flash").remote({
-      search = {
-        mode = "search",
-        max_length = 0,
-        forward = forward,
-        wrap = false,
-        multi_window = false,
-      },
-      label = {
-        after = { 0, 0 },
-      },
-      pattern = "^",
-      labels = "abcdefghijklmnopqrstuvwxyz",
-    })
-  end
-end
--- vim.utils.map("o", "l", flash_line(false))
--- vim.utils.map("o", "L", flash_line(true))
+vim.api.nvim_set_keymap("n", "öö", ";a_", { desc = "Add row surrounding" })
+vim.api.nvim_set_keymap("v", "öö", ";a", { desc = "Add surrounding" })
 
 -- Tab navigation
 vim.utils.map("n", { "<Tab>l", "<Tab><Tab>" }, "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
@@ -95,37 +71,3 @@ local mappings = {
 for key, direction in pairs(mappings) do
   vim.utils.map("nv", key, treewalker(direction), { noremap = true, silent = true })
 end
-
--- Window navigation
--- vim.utils.map("n", "<C-M-h>", "<C-w>h", { noremap = true, silent = true })
--- vim.utils.map("n", "<C-M-j>", "<C-w>j", { noremap = true, silent = true })
--- vim.utils.map("n", "<C-M-l>", "<C-w>l", { noremap = true, silent = true })
--- vim.utils.map("n", "<C-M-k>", "<C-w>k", { noremap = true, silent = true })
-
--- Text-Object Shortcut Custom pending-states mapping
--- local symbols = { "q", "b", "t", "[", "]", "<", ">", "(", ")", "{", "}" }
--- local symbols = { "q", "[", "]", "<", ">", "(", ")" }
--- for _, s in ipairs(symbols) do
---   vim.api.nvim_set_keymap("o", "l" .. s, "il" .. s, { silent = true })
---   vim.api.nvim_set_keymap("o", "l" .. string.upper(s), "al" .. s, { silent = true })
---
---   vim.api.nvim_set_keymap("o", "n" .. s, "in" .. s, { silent = true })
---   vim.api.nvim_set_keymap("o", "n" .. string.upper(s), "an" .. s, { silent = true })
---
---   vim.api.nvim_set_keymap("o", s, "i" .. s, { silent = true })
---   -- vim.api.nvim_set_keymap("o", string.upper(s), "a" .. s, { silent = true })
--- end
-
--- vim.utils.map("n", "<M-i>q", function()
---   vim.utils.trigger_keys("vinq<Esc>i")
--- end, { noremap = true, silent = true })
-
--- local function enter_pending_mode()
---   local key = vim.fn.getchar()
---   if key == vim.fn.char2nr("q") then
---     vim.utils.trigger_keys("vinq<Esc>i")
---   end
--- end
---
--- -- Map <M-i> to enter the pending mode
--- vim.utils.map("n", "<M-i>", enter_pending_mode, { noremap = true, silent = true })
