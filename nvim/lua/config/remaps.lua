8a802bb9f08dfc25eaedaa2329e7b0c162b479fa
Snuffish <snuffish90@gmail.com>
2025-01-18 02:45:23 +0100

    wip

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -10,6 +10,8 @@ vim.utils.map(
   { noremap = true, silent = true, desc = "Notifications" }
 )
 
+vim.api.nvim_set_keymap("", "å", "[", { silent = true })
+
 vim.api.nvim_set_keymap("", "m", "%", { silent = true })
 vim.api.nvim_set_keymap("", "M", "%", { silent = true })
 
