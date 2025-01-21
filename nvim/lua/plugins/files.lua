3e5ce41fb324f28228824271700702bb7df8fdcb
Snuffish <snuffish90@gmail.com>
2025-01-22 00:01:45 +0100

    wip

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -1,3 +1,66 @@
+local harpoon_modal = function()
+  local harpoon = require("harpoon")
+  local Menu = require("nui.menu")
+  local event = require("nui.utils.autocmd").event
+
+  local fileId = 1
+  local menu_items = {}
+  for _, item in ipairs(harpoon:list().items) do
+    if item.value ~= "" then
+      table.insert(
+        menu_items,
+        Menu.item(item.value, {
+          fileId = fileId,
+        })
+      )
+
+      fileId = fileId + 1
+    end
+  end
+
+  P(menu_items)
+
+  local menu = Menu({
+    position = "50%",
+    -- relative = "cursor",
+    size = {
+      width = 100,
+      height = 5,
+    },
+    border = {
+      style = "single",
+      text = {
+        top = "Buffers",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:Normal",
+    },
+  }, {
+    lines = menu_items,
+    max_width = 20,
+    keymap = {
+      focus_next = { "j", "<Down>", "<Tab>" },
+      focus_prev = { "k", "<Up>", "<S-Tab>" },
+      close = {
+        { "<Esc>", "<C-e>" },
+        { "<Esc>", "q" },
+      },
+      submit = { "<CR>", "<Space>" },
+    },
+    on_close = function()
+      print("Menu Closed!")
+    end,
+    on_submit = function(item)
+      local fileId = item.fileId
+      harpoon:list():select(fileId)
+    end,
+  })
+
+  menu:mount()
+end
+
 return {
   {
     "ThePrimeagen/harpoon",
@@ -11,66 +74,7 @@ return {
       harpoon:setup()
 
       vim.keymap.set("n", "<C-e>", function()
-        local Menu = require("nui.menu")
-        local event = require("nui.utils.autocmd").event
-
-        local fileId = 1
-        local menu_items = {}
-        for _, item in ipairs(harpoon:list().items) do
-          if item.value ~= "" then
-            table.insert(
-              menu_items,
-              Menu.item(item.value, {
-                fileId = fileId,
-              })
-            )
-
-            fileId = fileId + 1
-          end
-        end
-
-        P(menu_items)
-
-        local menu = Menu({
-          position = "50%",
-          -- relative = "cursor",
-          size = {
-            width = 100,
-            height = 5,
-          },
-          border = {
-            style = "single",
-            text = {
-              top = "Buffers",
-              top_align = "center",
-            },
-          },
-          win_options = {
-            winhighlight = "Normal:Normal,FloatBorder:Normal",
-          },
-        }, {
-          lines = menu_items,
-          max_width = 20,
-          keymap = {
-            focus_next = { "j", "<Down>", "<Tab>" },
-            focus_prev = { "k", "<Up>", "<S-Tab>" },
-            close = {
-              { "<Esc>", "<C-e>" },
-              { "<Esc>", "q" },
-            },
-            submit = { "<CR>", "<Space>" },
-          },
-          on_close = function()
-            print("Menu Closed!")
-          end,
-          on_submit = function(item)
-            local fileId = item.fileId
-            harpoon:list():select(fileId)
-          end,
-        })
-
-        -- mount the component
-        menu:mount()
+        harpoon_modal()
       end)
 
       vim.keymap.set("n", "<leader>a", function()
