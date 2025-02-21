a061e8b8821813b2af2842c49bdbe2f7564cd4f1
Snuffish <snuffish90@gmail.com>
2025-02-21 08:59:16 +0100

    wip

--- vim.bash
+++ vim.bash
@@ -3,6 +3,7 @@
 alias .nvim="cd \$HOME/.config/nvim"
 alias .v=".nvim"
 alias v="nvim"
+alias nv="neovide"
 alias vim="nvim"
 
 alias vf="nvim \$(fzf --preview 'bat --color=always {}' --preview-window '~3')"
