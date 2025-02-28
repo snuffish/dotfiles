9a4bbce17f9f846e3d02d4a5cd7146c1b4aceeae
Snuffish <snuffish90@gmail.com>
2025-02-28 20:35:48 +0100

    wip

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -205,4 +205,13 @@ return {
       { "]q", false },
     },
   },
+  {
+    "lafarr/hierarchy.nvim",
+    opts = {
+      -- depths = 10,
+    },
+    keys = {
+      { "gt", mode = { "n" }, "<cmd>FunctionReferences<CR>", desc = "FunctionReferences" },
+    },
+  },
 }
