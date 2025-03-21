c741713e9bf88d2fe933336cb73fa5c1adb8f484
Snuffish <snuffish90@gmail.com>
2025-03-21 12:00:35 +0100

    wip

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,13 +1,18 @@
-local lfs = require("lfs")
+require("config.autocmds.lsp")
+require("config.autocmds.filetype")
+require("config.autocmds.cursor")
 
-local function require_all_autocmds()
-  local path = vim.fn.stdpath("config") .. "/lua/config/autocmds"
-  for file in lfs.dir(path) do
-    if file:match("%.lua$") then
-      local module_name = "config.autocmds." .. file:gsub("%.lua$", "")
-      require(module_name)
-    end
-  end
-end
+-- local lfs = require("lfs")
+--
+-- local function require_all_autocmds()
+--   local path = vim.fn.stdpath("config") .. "/lua/config/autocmds"
+--   for file in lfs.dir(path) do
+--     if file:match("%.lua$") then
+--       local module_name = "config.autocmds." .. file:gsub("%.lua$", "")
+--       require(module_name)
+--     end
+--   end
+-- end
+--
+-- require_all_autocmds()
 
-require_all_autocmds()
