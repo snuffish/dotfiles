6b63ed880ab3bc33f05d16e81868bf2ce454f0be
Christoffer Engman <snuffish90@gmail.com>
2025-08-20 15:11:41 +0200

    wip

---
+++ meligo.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WEBAPP_NAME="webApp-test1296bc4c"
+RESORUCE_GROUP="rg-testc7776f6f"
+alias meligo_test_tail="az webapp log tail --name $WEBAPP_NAME --resource-group $RESORUCE_GROUP"
+
