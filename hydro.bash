b19142562c5c7d71f4c41b74372fc45f8d330e10
Christoffer Engman <christoffer.engman@hydro.com>
2025-10-28 15:14:01 +0100

    wip

--- hydro.bash
+++ hydro.bash
@@ -2,4 +2,4 @@
 
 alias prod_logs_dir="cd /c/Heureka/logs/springboot/central"
 
-alias tailprod="tail -f '//vtl-01-ms055/logs/springboot/central/app.log'"
+alias tailprod="tail -f '//vtl00-ms055/logs/springboot/central'"
