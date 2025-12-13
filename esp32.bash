b3e9be276d3e64aa68183299e6ec6fc13b7c9e11
Snuffish <snuffish90@gmail.com>
2025-12-13 08:48:14 +0100

    wip

--- esp32.bash
+++ esp32.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+autoload -U compinit
+compinit
+
 function venv() {
-  autoload -U compinit
-  compinit
+  python -m venv venv
+  source venv/bin/activate
 
   eval "$(_ESPTOOL_COMPLETE=zsh_source esptool)"
   eval "$(_ESPSECURE_COMPLETE=zsh_source espsecure)"
   eval "$(_ESPEFUSE_COMPLETE=zsh_source espefuse)"
 }
-
