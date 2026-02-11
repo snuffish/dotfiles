ec117452fe47b8cf49628f548869bf15bce3dfd9
Christoffer Engman <snuffish90@gmail.com>
2026-02-11 11:25:10 +0100

    wip

--- tmux/init.bash
+++ tmux/init.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-alias tm="tmux attach -t \$(tmux list-sessions | fzf --preview 'echo \"<TODO>\"' --height 50% --layout reverse | sed 's/: .*//g')"
-alias tmnew="tmux new -s"
-alias tmattach="tmux attach -t"
-alias tmkill="tmux kill-session -t"
+alias tm="tmux attach -t \$(tmux list-sessions | fzf --header 'Select tmux session to open:' --preview-window='right:70%:follow' --preview 'tmux capture-pane -ept \$(echo {} | cut -d: -f1)' --height 80% --layout reverse | sed 's/: .*//g')"
+alias tmn="tmux new -s"
+alias tmk="tmux kill-session -t \$(tmux list-sessions | fzf --header 'Select tmux session to kill:' --preview-window='right:70%:follow' --preview 'tmux capture-pane -ept \$(echo {} | cut -d: -f1)' --height 80% --layout reverse  | sed 's/: .*//g')"
