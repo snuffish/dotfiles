3550652f1ddbca6da27d627ae97a2fcbbc08276c
Christoffer Engman <snuffish90@gmail.com>
2026-02-21 07:51:24 +0100

    wip

--- azure.bash
+++ azure.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-alias az_beijer-login="az login --tenant \$SECRET_BEIJER_ELECTRONICS_TENANT_ID"
+alias az_beijer-login="az login --tenant 4b2e9b91-de77-4ca7-8130-c80faee67059"
 alias az_meligo-login="az login --tenant 387f7c11-57ea-4bc5-b147-3aeaef8fd4e4"
