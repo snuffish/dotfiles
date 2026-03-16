7d09d1a5683a796555de16f349c209c97c455ab1
Christoffer Engman <snuffish90@gmail.com>
2026-03-16 15:46:15 +0100

    wip

--- azure.bash
+++ azure.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-alias az_beijer-login="az login --tenant 4b2e9b91-de77-4ca7-8130-c80faee67059"
-alias az_meligo-login="az login --tenant 387f7c11-57ea-4bc5-b147-3aeaef8fd4e4"
+alias az_beijer-login="az login --tenant $BEIJER_TENANT_ID"
+alias az_meligo-login="az login --tenant $MELIGO_TENANT_ID"
+
