47a15f4ccd89b2e232b9bab5d5d06f3231c1aced
Snuffish <snuffish90@gmail.com>
2026-05-03 21:14:30 +0200

    wip

--- filesystem.bash
+++ filesystem.bash
@@ -42,6 +42,4 @@ alias .ghostty='cd "$HOME/Library/Application Support/com.mitchellh.ghostty"'
 
 alias space="du -d1 -h 2>/dev/null | sort -h"
 
-alias diskspace='df -h -x squashfs -x tmpfs -x devtmpfs | grep -E --color=never "Filesystem|disken|/$"
-'
-
+alias diskspace='df -h -x squashfs -x tmpfs -x devtmpfs | grep -E --color=never "Filesystem|disken|/$"'
