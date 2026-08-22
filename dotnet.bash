d5dc9444c4729f1138307b39ececfdb2051d4c71
Snuffish <snuffish90@gmail.com>
2026-08-22 22:45:56 +0200

    x

--- dotnet.bash
+++ dotnet.bash
@@ -3,3 +3,6 @@
 alias dnr="dotnet run"
 alias dnb="dotnet build"
 alias dnt="dotnet test"
+
+# VVS NUGET PAT
+export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS='{"endpointCredentials":[{"endpoint":"https://grutbildning.pkgs.visualstudio.com/_packaging/GR.Library/nuget/v3/index.json","username":"pat","password":"$GRUTBILDNING_NUGET_ACCESS_TOKEN"}]}'
